a9095a1a53e350d6ba394ba95ab6aecf83cee2ac
Alex <akong@gwmail.gwu.edu>
2019-02-26 19:01:55 -0800

    update kotlin version and add list frag and book model

---
+++ app/src/main/java/com/example/trendingbooks/Book.kt
@@ -0,0 +1,9 @@
+package com.example.trendingbooks
+
+
+class Book (val id: Int,
+            val title: String,
+            val authorFirstName: String,
+            val authorLastName: String,
+            val marketingMessage: String?,
+            val synopsis: String)
---
+++ app/src/main/java/com/example/trendingbooks/BooksListFragment.kt
@@ -0,0 +1,9 @@
+package com.example.trendingbooks
+
+import android.support.v4.app.Fragment
+
+
+class BooksListFragment: Fragment() {
+
+
+}
